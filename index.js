0c2895e3306daa90a0ecca0344f42a857574f3f3
Andrew Kennedy <akennedycode@gmail.com>
2020-08-10 17:29:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function dwarfRollCall(dwarves) {
-}
 
+var dwarves = ["Dopey", "Grumpy", "Bashful"]
+for (let i=0, d=dwarves.length; i<d; i++){
+dwarves.push('${i+1}. ${dwarves[i]}')
+}
+return dwarves
+}
 function summonCaptainPlanet(planeteerCalls){
 }
 
